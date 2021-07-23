8e6babe1d21e3972ea0512ba254e9b18162f6284
AxR <74997285+AxRRR@users.noreply.github.com>
2021-07-23 09:55:42 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,83 +1,87 @@
 function getInfo(type) {
   switch (type) {
-        case 'history':
-            return document.getElementById("history").innerText;
-        case 'result':
-            return document.getElementById("result").innerText;
-
-    default:
-        break;
-  }
-}
-
-function refreshInfo(type, number) {
-  switch (type) {
-        case 'history':
-            return document.getElementById("history").innerText = number;
-
-        case 'result':
-            if(number == ""){
-                return document.getElementById("result").innerText = "";
-            }
-            else{ 
-                return document.getElementById("result").innerText = number;
-            }
+    case "history":
+      return document.getElementById("history").innerText;
+    case "result":
+      return document.getElementById("result").innerText;
 
     default:
       break;
   }
 }
 
-function getNumber() {
-    return number = document.getElementsByName("number");
-}
+function refreshInfo(type, number) {
+  switch (type) {
+    case "history":
+      return (document.getElementById("history").innerText = number);
 
-function getOperator() {
-    return operator = document.getElementsByName("operator");
+    case "result":
+      if (number == "") {
+        return (document.getElementById("result").innerText = "");
+      } else {
+        return (document.getElementById("result").innerText = number);
+      }
+
+    default:
+      break;
+  }
 }
 
 var operator = document.getElementsByName("operator");
-    for (let i = 0; i < operator.length; i++) {
-        operator[i].addEventListener('click', () => onClickOperator(operator[i]));
-    }
+for (let i = 0; i < operator.length; i++) {
+  operator[i].addEventListener("click", () => onClickOperator(operator[i]));
+}
 
 var number = document.getElementsByName("number");
-    for (let i = 0; i < number.length; i++) {
-        number[i].addEventListener('click', () => onClickNumber(number[i]));
+for (let i = 0; i < number.length; i++) {
+  number[i].addEventListener("click", () => onClickNumber(number[i]));
+}
+
+function onClickOperator(operator) {
+  const getResult = getInfo("result");
+  const getHistory = getInfo("history");
+  const isResultShow = false;
+
+  if (operator.id == "clear") {
+    refreshInfo("result", "");
+    refreshInfo("history", "");
+    return;
+  }
+  else if(operator.id == "backspace"){
+    res = getResult.slice(0, -1);
+    refreshInfo("result", res);
+  }
+  else if(operator.id == "%"){
+    let res = eval(getResult/100)
+    return refreshInfo("result", res)
+  }
+  else if (operator.id == "=") {
+    try {
+      let res = eval(getResult);
+      console.log(res);
+      refreshInfo("result", res);
+      refreshInfo("history", getResult);
+    } catch (error) {
+      refreshInfo("result", "Syntax Error");
     }
-
-    function reverseNumberFormat(num){
-        return Number(num.replace(/,/g,''));
+  } else if (operator.id) {
+    return refreshInfo("result", getResult + operator.id);
+  } else if (getResult != "" && getHistory != "") {
+    if (isResultShow == true) {
+      refreshInfo("result", getResult);
+    } else {
+      refreshInfo("result", getResult + operator.id + getResult);
     }
+  }
+}
 
-    function onClickOperator(operator) {
-        const getNumber = getInfo('result');
-        const getHistory = getInfo('history');
+function onClickNumber(num) {
+  const getResult = getInfo("result");
+  const getHistory = getInfo("history");
 
-        if(operator.id == "clear") {
-            refreshInfo('history', "")
-            refreshInfo('result', "")
-        }
-        else if(operator.id == "=") {
-            let res = eval(getNumber);
-            console.log(res)
-            refreshInfo('result', res)
-        }
-        else if(getNumber != "" && getHistory != ""){
-            console.log('aca estamos en el if')
-            refreshInfo('result', getHistory+operator.id+getNumber)
-        }
-    }
-
-    function onClickNumber(num) {
-        const getNumber = getInfo('result');
-        const getHistory = getInfo('history');
-        // console.log(getNumber)
-        if(getNumber == "" && getHistory == "") {
-            refreshInfo('result', num.id)
-        } else {
-            refreshInfo('history', getNumber);
-            refreshInfo('result', num.id)
-        }
-    }
-
+  if (getResult == "" && getHistory == "") {
+    refreshInfo("result", getResult + num.id);
+  } else {
+    refreshInfo("result", getResult + num.id);
+  }
+}
