f17667d3a70f026b90ae6e1e711943a0820bcdf5
AxR <alexfabrisiolunatoledo@gmail.com>
2021-07-23 08:19:34 -0500

    Fixed somethings

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function getInfo(type) {
 function refreshInfo(type, number) {
   switch (type) {
         case 'history':
-            document.getElementById("history").innerText = number;
+            return document.getElementById("history").innerText = number;
 
         case 'result':
             if(number == ""){
@@ -32,23 +32,52 @@ function getNumber() {
     return number = document.getElementsByName("number");
 }
 
-// function getOperator() {
-//     return operator = document.getElementsByName("operator");
-// }
+function getOperator() {
+    return operator = document.getElementsByName("operator");
+}
 
 var operator = document.getElementsByName("operator");
     for (let i = 0; i < operator.length; i++) {
         operator[i].addEventListener('click', () => onClickOperator(operator[i]));
     }
 
-function onClickOperator(operator) {
-    // console.log(button)
-    if(operator.id == "clear") {
-        refreshInfo('history', "")
-        refreshInfo('result', "")
+var number = document.getElementsByName("number");
+    for (let i = 0; i < number.length; i++) {
+        number[i].addEventListener('click', () => onClickNumber(number[i]));
     }
-}
 
+    function reverseNumberFormat(num){
+        return Number(num.replace(/,/g,''));
+    }
 
-// getOperator();
-// // getNumber();
+    function onClickOperator(operator) {
+        const getNumber = getInfo('result');
+        const getHistory = getInfo('history');
+
+        if(operator.id == "clear") {
+            refreshInfo('history', "")
+            refreshInfo('result', "")
+        }
+        else if(operator.id == "=") {
+            let res = eval(getNumber);
+            console.log(res)
+            refreshInfo('result', res)
+        }
+        else if(getNumber != "" && getHistory != ""){
+            console.log('aca estamos en el if')
+            refreshInfo('result', getHistory+operator.id+getNumber)
+        }
+    }
+
+    function onClickNumber(num) {
+        const getNumber = getInfo('result');
+        const getHistory = getInfo('history');
+        // console.log(getNumber)
+        if(getNumber == "" && getHistory == "") {
+            refreshInfo('result', num.id)
+        } else {
+            refreshInfo('history', getNumber);
+            refreshInfo('result', num.id)
+        }
+    }
+
